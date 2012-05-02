f58fb7536e5f8377d7c01a47b9bdfcfbb8df7507
Erik Loyer <erik@song.nu>
2012-05-02 15:48:06 -0700

    Initial Commit

---
+++ MimesisTouch/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  MimesisTouch
+//
+//  Created by Erik Loyer on 5/2/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
